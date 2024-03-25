4b4658ee8fb6e03f3b4ca8a415b25b2328e07396
rafaellopes21 <rafaellopes1025@gmail.com>
2024-03-25 11:01:21 -0300

    Finished the routes to manipulate the filemanager

--- resources/views/home/index.php
+++ resources/views/home/index.php
@@ -1,8 +1,3 @@
 <?php ?>
 <div class="breadcrumb-main"><?= import('_layouts/menus/header'); ?></div>
-
-<ol>
-    <li>Quando o projeto terminar, copie o arquivo default.json e renomeio para english.json o arquivo copiado.</li>
-</ol>
-
 <?= import('list/index.php'); ?>
